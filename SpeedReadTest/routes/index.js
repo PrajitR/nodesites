365b845de7cc16a500c39b0d62f5f40f6ea58366
Prajit Ramachandran <ramprajit@gmail.com>
2014-04-09 15:12:36 -0700

    Lots of fixups + finished views

--- SpeedReadTest/routes/index.js
+++ SpeedReadTest/routes/index.js
@@ -16,6 +16,7 @@ module.exports = function (app) {
       md5.update(String(Date.now()) + Math.random());
       var digest = md5.digest('hex');
       res.cookie('id', digest, { maxAge: 1000 * 60 * 60 * 24 * 7 });
+
       pg.connect(db, function (err, client, done) {
         if(err) return dealWithError('Postgres error: ' + err, res);
         client.query('INSERT INTO speedread (user_id) VALUES ($1)',
@@ -44,7 +45,7 @@ module.exports = function (app) {
         content[testname] = testContent;
         fs.readFile(questionPath, 'utf8', function (err, testQuestions) {
           if (err) dealWithError(err, res);
-          testQuestions = testQuestions.split('---');
+          testQuestions = testQuestions.split('\n\n');
           questions[testname] = testQuestions;
 
           res.render('test', { testname: testname, 
@@ -61,38 +62,46 @@ module.exports = function (app) {
     var testname = req.params.test;
     if (validTests.indexOf(testname) == -1)
       dealWithError('No such test: ' + testname, res);
-    var answers = req.body.answers,
-        wpm = req.body.wpm,
-        percentCorrect = gradeAnswers(testname, answers);
+    var answers = [],
+        wpm = Math.min(Math.round(Number(req.body.wpm)), 32767);
+    for (var i = 0; i < 5; i++) {
+      answers[i] = req.body['q' + i];
+    }
+    var percentCorrect = gradeAnswers(testname, answers);
     // add to database
     pg.connect(db, function (err, client, done) {
-      if(err) return dealWithError('Postgres error: ' + err);
+      if(err) return dealWithError('Postgres error: ' + err, res);
       client.query('UPDATE speedread SET ' + testname + ' = ($1), wpm_' +
         testname + ' = ($2) WHERE user_id = ($3)',
         [percentCorrect, wpm, req.cookies.id], function (err, result) {
-          if(err) return dealWithError('Postgres error: ' + err);
+          if(err) return dealWithError('Postgres error: ' + err, res);
           done();
+          if (testname == 'speedfiction') {
+            res.redirect('/test/regfiction');
+          } else {
+            res.redirect('/results');
+          }
         });
     });
   });
 
-  function gradeAnswers (testname, answers) {
+  function gradeAnswers (testname, userAnswers) {
     if (!answers[testname]) {
       var answerPath = path.join(__dirname, '..', 'data', 'answers', testname + '.txt'),
       testAnswers = fs.readFileSync(answerPath, 'utf8'); // sync is simpler
       testAnswers = testAnswers.split(' ');
       answers[testname] = testAnswers;
-      return grade(answers[testname]);
-    } else {
-      return grade(answers[testname]);
     }
+    return grade(answers[testname]);
 
     function grade (correct) {
       var numCorrect = 0;
-      for (var i = 0; i < answers.length; i++) {
-        numCorrect += (answers[i] == correct[i]);
+      for (var i = 0; i < userAnswers.length; i++) {
+        console.log(userAnswers[i] + ' vs. ' + correct[i]);
+        numCorrect += (userAnswers[i] == correct[i].trim());
       }
-      return 100 * numCorrect / answers.length;
+      console.log(numCorrect);
+      return 100 * numCorrect / userAnswers.length;
     }
   }
 
@@ -102,7 +111,7 @@ module.exports = function (app) {
       if (err) return dealWithError('Postgres error: ' + err, res);
       client.query('SELECT * FROM speedread WHERE user_id = ($1)',
         [req.cookies.id], function (err, result) {
-          if (err) return dealWithError('Postgres error: ' + err);
+          if (err) return dealWithError('Postgres error: ' + err, res);
           done();
           res.render('results', { user: result.rows[0] });
         });
